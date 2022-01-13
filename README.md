60219fa235b7c9a7db83800176c533addfbe7858
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-01-13 14:16:48 -0700

    form and event

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # taskinaor
+
+## Description
--- assets/js/script.js/script.js
+++ assets/js/script.js/script.js
@@ -1,13 +1,17 @@
 console.dir(window.document);
-var buttonEl = document.querySelector("#save-tasks");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
+
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
